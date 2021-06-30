68c12ac6abe4c1360de8db4298f91cd2ceeb9fce
FundationSemillas <86446293+FundationSemillas@users.noreply.github.com>
2021-06-30 09:30:08 -0500

    borrar imagenes mediante fk tabla album

--- BACKEND/app/Http/Controllers/ImageController.php
+++ BACKEND/app/Http/Controllers/ImageController.php
@@ -55,7 +55,7 @@ class ImageController extends Controller
 
                 // $id= 21;
                 $images = new Images();
-                    $images->name =  $picture;
+                    $images->image =  $picture;
                     $images->type =  $extension;
                     $images->description =  $description;
                     $images->albums_id = $id;
@@ -102,12 +102,12 @@ class ImageController extends Controller
         $data = $request->json()->all();
         
         $images = images::findOrFail($id);
-        $dataImages = $data['images'];
+        $dataImages = $data['image'];
         $dataAlbums = $data['album'];
         $album = Albums::findOrFail($dataAlbums['id']);
 
        
-        $images->name =  $dataImages['name'];
+        $images->image =  $dataImages['image'];
         $images->type =  $dataImages['type'];
         $images->description =  $dataImages['description'];
         $images->album()->associate($album);
@@ -128,7 +128,8 @@ class ImageController extends Controller
     public function destroy(images $images,$id)
     {
         $images = images::findOrFail($id);
-        $images->delete();
+        $idfk = $images['albums_id'];
+        DB::table('images')->where('albums_id', $idfk)->delete();
         return response()->json(['message'=>'images quitado', 'images'=>$images],200);
     }
 }

560442764fc7dd6cc6f451b36d516deef4d215df
FundationSemillas <86446293+FundationSemillas@users.noreply.github.com>
2021-07-12 16:26:58 -0500

    lógica de controladores

--- BACKEND/app/Http/Controllers/UserController.php
+++ BACKEND/app/Http/Controllers/UserController.php
@@ -19,11 +19,35 @@ public function user(Request $request){
   public function store(){
       //
   }
-  public function update(){
-      //
+    
+   public function show($id)
+  {
+      $user = user::findOrFail($id);
+      return response()->json(
+            $user
+     );
   }
-  public function destroy(){
-      //
+    
+  public function update(Request $request, $id){
+    $data = $request->json()->all();
+    $user = user::findOrFail($id);
+    $user->name =  $data['name'];
+    $user->last_name =  $data['last_name'];
+    $user->email =  $data['email'];
+    $user->password =  Hash::make($data['password']);
+    $user->save();
+    return response()->json([
+        'data' => [
+            'Actualizado' => 'Exitoso'
+        ]
+    ], 201);
+  }
+    
+   public function destroy($id)
+  {
+      $user = user::findOrFail($id);
+      $user->delete();
+      return response()->json(['message' => 'Usuario Eliminado', 'usuarios' => $user], 200);
   }
   
 
